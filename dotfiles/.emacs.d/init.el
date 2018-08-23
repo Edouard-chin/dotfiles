ad6b6d168d7a63315c51d5807b777cdd7f79ac0f
Edouard CHIN <edouard.chin@shopify.com>
2018-08-23 01:34:30 -0400

    Clean config, thanks bbatsov

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,90 +1,21 @@
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
 
-(load-theme 'zenburn t)
+;; Start emacs maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; Disable arrow keys
-(global-unset-key (kbd "<left>"))
-(global-unset-key (kbd "<right>"))
-(global-unset-key (kbd "<up>"))
-(global-unset-key (kbd "<down>"))
-
-;; Enable line number and add margin
-(global-linum-mode 1)
-
-(custom-set-variables
- '(package-selected-packages
-   (quote
-    (flx-ido projectile auto-complete expand-region magit zenburn-theme))))
-
+;; Cursor and region customization
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "systemYellowColor" :foreground "#DCDCCC"))))
  '(region ((t (:background "Orange")))))
-
-;; Run ruby test at point, this requires the rails test runner and `dev` (Shopify)
-(defun run-test-at-point()
- (interactive)
- (setq default-directory (locate-dominating-file buffer-file-name "Gemfile"))
-
- (setq shell-command-switch "-ic")
- (async-shell-command (format "dev test %s:%d" (buffer-file-name) (line-number-at-pos)))
-)
-(global-set-key (kbd "C-x t") 'run-test-at-point)
-
-;; Comment line
-(global-set-key (kbd "C-x C-_") 'comment-line)
-
-;; Use the expand region package
-(require 'expand-region)
-(global-set-key (kbd "C-x C-t") 'er/expand-region)
-
-;; Enable ido mode everywhere with flex matching
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;; Rebing C-x C-b for ibuffer instead of the vanilla buffer list
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; Enable winner mode (which allows to changes the window configuration)
-(winner-mode 1)
-
-;; Find file
-(global-set-key (kbd "C-x p") 'find-name-dired)
-
-;; Kill current buffer without having to confirm
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-;; Disable the bell ding
-(setq ring-bell-function 'ignore)
-
-;; Enable whitespace visualization
-(setq whitespace-style '(face tabs spaces empty lines-tail trailing))
-(global-whitespace-mode t)
-(setq whitespace-line-column 120)
-
-;; Disable ruby mode to add the encoding utf8 comment when saving
-(setq ruby-insert-encoding-magic-comment nil)
-
-;; Enable autocompletion
-(global-auto-complete-mode t)
 
 ;; Font and text customization
 (set-face-attribute 'default nil
@@ -101,8 +32,39 @@
 ;; Remove the scrollbar (window mode)
 (scroll-bar-mode -1)
 
-;; Start emacs maximized
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; Disable the bell ding
+(setq ring-bell-function 'ignore)
+
+;; Disable arrow keys
+(global-unset-key (kbd "<left>"))
+(global-unset-key (kbd "<right>"))
+(global-unset-key (kbd "<up>"))
+(global-unset-key (kbd "<down>"))
+
+;; Enable line number number mode
+(global-linum-mode 1)
+(column-number-mode t)
+
+;; revert buffers automatically when underlying files are changed externally
+(global-auto-revert-mode t)
+
+;; Enable winner mode (which allows to changes the window configuration)
+(winner-mode 1)
+
+;; highlight the current line
+(global-hl-line-mode +1)
+
+;; Automatically closes pair like "''" or "{}"
+(electric-pair-mode t)
+
+;; Rebing C-x C-b for ibuffer instead of the vanilla buffer list
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+;; Find file
+(global-set-key (kbd "C-x p") 'find-name-dired)
+
+;; Kill current buffer without having to confirm
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 ;; Add a hook so that `compile` executes ruby when in ruby-mode
 (add-hook 'ruby-mode-hook
@@ -110,19 +72,96 @@
             (set (make-local-variable 'compile-command)
                  (concat "ruby " buffer-file-name))))
 
-;; Compile shortcut
-(global-set-key (kbd "C-c e") 'compile)
-
 ;; Adds a newline at EOF when file is saved
 (setq require-final-newline t)
-
-;; Automatically closes pair like "''" or "{}"
-(electric-pair-mode t)
-
-;; Enable CUA selection mode for rectangle selection improvement
-(cua-selection-mode t)
 
 ;; Write backup files to own directory
 (setq backup-directory-alist
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.5)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (global-company-mode))
+
+(use-package zenburn-theme
+  :ensure t
+  :config
+  (load-theme 'zenburn t))
+
+(use-package ace-window
+  :ensure t
+  :bind (([remap other-window] . ace-window)))
+
+(use-package paren
+  :config
+  (show-paren-mode +1))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package ivy
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t))
+
+(use-package swiper
+  :bind (("C-s" . swiper)))
+
+(use-package counsel
+  :ensure t
+  :bind (("C-c g" . counsel-git)
+         ("C-c j" . counsel-git-grep)
+         ("C-c r" . counsel-rg)
+         ("C-x l" . counsel-locate)))
+
+(use-package avy
+  :ensure t
+  :bind (("C-'" . avy-goto-char))
+  :config
+  (setq avy-background t))
+
+(use-package ruby-mode
+  :config
+  (setq ruby-insert-encoding-magic-comment nil)
+  (add-hook 'ruby-mode-hook #'subword-mode))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-x C-t" . er/expand-region))
+
+(use-package whitespace
+  :init
+  (dolist (hook '(prog-mode-hook text-mode-hook))
+    (add-hook hook #'whitespace-mode))
+  (add-hook 'before-save-hook #'whitespace-cleanup)
+  :config
+  (setq whitespace-style '(face tabs spaces empty lines-tail trailing))
+  (setq whitespace-line-column 120))
+
+;; Run ruby test at point, this requires the rails test runner and `dev` (Shopify)
+(defun run-test-at-point()
+ (interactive)
+ (setq default-directory (locate-dominating-file buffer-file-name "Gemfile"))
+
+ (setq shell-command-switch "-ic")
+ (async-shell-command (format "dev test %s:%d" (buffer-file-name) (line-number-at-pos)))
+)
+(global-set-key (kbd "C-x t") 'run-test-at-point)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company zenburn-theme yaml-mode use-package golden-ratio expand-region counsel auto-complete ace-window))))
